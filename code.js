3af03c41b220090346c399e7cce2b19c3c89309d
Nitin Rangarajan <nitinr92@gmail.com>
2019-11-21 16:34:27 -0500

    Added colors to Elipsis

--- code.js
+++ code.js
@@ -11,24 +11,70 @@ var iFrameProps = {
     width: 260,
     height: 300
 };
+const statusColors = {
+    Active: {
+        r: 24,
+        g: 160,
+        b: 251
+    },
+    "In-review": {
+        r: 255,
+        g: 199,
+        b: 0
+    },
+    "Needs changes": {
+        r: 242,
+        g: 72,
+        b: 34
+    },
+    Approved: {
+        r: 27,
+        g: 196,
+        b: 125
+    },
+    "In-development": {
+        r: 255,
+        g: 0,
+        b: 255
+    }
+};
 figma.showUI(__html__, iFrameProps);
-function generateText(status, ticketNumber) {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield figma.loadFontAsync({ family: "Roboto", style: "Regular" });
-        let mytext = figma.createText();
-        let ellipse = figma.createEllipse();
-        ellipse.resize(25, 25);
-        mytext.characters = status + " " + ticketNumber;
-        figma.currentPage.appendChild(ellipse);
-        figma.currentPage.appendChild(mytext);
-    });
-}
-figma.ui.onmessage = (msg) => {
-    if (msg.type === "apply_status") {
+const clone = val => {
+    return JSON.parse(JSON.stringify(val));
+};
+const setColors = (fills, color) => {
+    fills[0].color.r = color.r;
+    fills[0].color.g = color.g;
+    fills[0].color.b = color.b;
+    return fills;
+};
+const generateStatus = (status, color, ticketNumber) => __awaiter(this, void 0, void 0, function* () {
+    yield figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+    let mytext = figma.createText();
+    let ellipse = figma.createEllipse();
+    ellipse.resize(15, 15);
+    ellipse.fills = [
+        {
+            type: "SOLID",
+            color: {
+                r: color.r / 255,
+                g: color.g / 255,
+                b: color.b / 255
+            }
+        }
+    ];
+    mytext.characters = status + " " + ticketNumber;
+    mytext.fontSize = 9;
+    figma.currentPage.appendChild(ellipse);
+    figma.currentPage.appendChild(mytext);
+});
+figma.ui.onmessage = (param) => {
+    if (param.type === "apply_status") {
         figma.currentPage.selection.forEach(node => {
-            generateText(msg.message.status, msg.message.ticketNumber);
+            var color = statusColors[param.message.status];
+            generateStatus(param.message.status, color, param.message.ticketNumber);
             if (node.name) {
-                node.name = msg.message.status + " " + msg.message.ticketNumber;
+                node.name = param.message.status + " " + param.message.ticketNumber;
             }
         });
     }
--- code.ts
+++ code.ts
@@ -7,30 +7,97 @@ interface IPluginMessage {
   message: IMessageType;
 }
 
+interface IColorIndex {
+  r: number;
+  g: number;
+  b: number;
+}
+
+interface IStatusColors {
+  [propName: string]: IColorIndex;
+}
+
 var iFrameProps: ShowUIOptions = {
   width: 260,
   height: 300
 };
 
+const statusColors: IStatusColors = {
+  Active: {
+    r: 24,
+    g: 160,
+    b: 251
+  },
+  "In-review": {
+    r: 255,
+    g: 199,
+    b: 0
+  },
+  "Needs changes": {
+    r: 242,
+    g: 72,
+    b: 34
+  },
+  Approved: {
+    r: 27,
+    g: 196,
+    b: 125
+  },
+  "In-development": {
+    r: 255,
+    g: 0,
+    b: 255
+  }
+};
+
 figma.showUI(__html__, iFrameProps);
 
-async function generateText(status: string, ticketNumber?: string) {
+const clone = val => {
+  return JSON.parse(JSON.stringify(val));
+};
+
+const setColors = (fills: any, color: IColorIndex) => {
+  fills[0].color.r = color.r;
+  fills[0].color.g = color.g;
+  fills[0].color.b = color.b;
+  return fills;
+};
+
+const generateStatus = async (
+  status: string,
+  color: IColorIndex,
+  ticketNumber?: string
+) => {
   await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
   let mytext = figma.createText();
   let ellipse = figma.createEllipse();
-  ellipse.resize(25, 25);
+  ellipse.resize(15, 15);
+
+  ellipse.fills = [
+    {
+      type: "SOLID",
+      color: {
+        r: color.r / 255,
+        g: color.g / 255,
+        b: color.b / 255
+      }
+    }
+  ];
+
   mytext.characters = status + " " + ticketNumber;
+  mytext.fontSize = 9;
   figma.currentPage.appendChild(ellipse);
   figma.currentPage.appendChild(mytext);
-}
+};
 
-figma.ui.onmessage = (msg: IPluginMessage) => {
-  if (msg.type === "apply_status") {
+figma.ui.onmessage = (param: IPluginMessage) => {
+  if (param.type === "apply_status") {
     figma.currentPage.selection.forEach(node => {
-      generateText(msg.message.status, msg.message.ticketNumber);
+      var color = statusColors[param.message.status];
+      generateStatus(param.message.status, color, param.message.ticketNumber);
 
       if (node.name) {
-        node.name = msg.message.status + " " + msg.message.ticketNumber;
+        node.name = param.message.status + " " + param.message.ticketNumber;
       }
     });
   }
