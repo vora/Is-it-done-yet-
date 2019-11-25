e34a58f5b59f2221eded85df89ecdd82cc966cc9
Nitin Rangarajan <nitinr92@gmail.com>
2019-11-25 16:53:45 -0500

    Added emoji-circle support

--- code.js
+++ code.js
@@ -1,93 +1,39 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var iFrameProps = {
+const iFrameProps = {
     width: 260,
     height: 300
 };
-const statusColors = {
-    Active: {
-        r: 24,
-        g: 160,
-        b: 251
-    },
-    "In-review": {
-        r: 255,
-        g: 199,
-        b: 0
-    },
-    "Needs changes": {
-        r: 242,
-        g: 72,
-        b: 34
-    },
-    Approved: {
-        r: 27,
-        g: 196,
-        b: 125
-    },
-    "In-development": {
-        r: 255,
-        g: 0,
-        b: 255
+const setStatusColor = (statusType) => {
+    var emojiHex = "";
+    switch (statusType) {
+        case "Active":
+            emojiHex = "\uD83D\uDD35";
+            break;
+        case "Approved":
+            emojiHex = "\uD83D\uDFE2";
+            break;
+        case "In-development":
+            emojiHex = "\uD83D\uDFE3";
+            break;
+        case "In-review":
+            emojiHex = "\uD83D\uDFE1";
+            break;
+        case "Needs changes":
+            emojiHex = "\uD83D\uDD34";
+            break;
     }
+    return emojiHex;
 };
 figma.showUI(__html__, iFrameProps);
-const setColors = (fills, color) => {
-    fills[0].color.r = color.r;
-    fills[0].color.g = color.g;
-    fills[0].color.b = color.b;
-    return fills;
-};
-const determineCoordinatesPosition = (node, ellipseNode, textNode) => {
-    var positionObj = {
-        ellipseNodeCoordinateX: node.x + node.width - textNode.width - ellipseNode.width - 1,
-        ellipseNodeCoordinateY: node.y - ellipseNode.height,
-        textNodeCoordianteX: node.x + node.width - textNode.width,
-        textNodeCoordinateY: node.y - textNode.height
-    };
-    return positionObj;
-};
-const generateStatus = (node, status, color, ticketNumber) => __awaiter(this, void 0, void 0, function* () {
-    yield figma.loadFontAsync({ family: "Roboto", style: "Regular" });
-    let textNode = figma.createText();
-    let ellipseNode = figma.createEllipse();
-    ellipseNode.resize(7, 7);
-    ellipseNode.fills = [
-        {
-            type: "SOLID",
-            color: {
-                r: color.r / 255,
-                g: color.g / 255,
-                b: color.b / 255
-            }
-        }
-    ];
-    textNode.characters = status + " " + ticketNumber;
-    textNode.fontSize = 6;
-    var coordinateObj = determineCoordinatesPosition(node, ellipseNode, textNode);
-    ellipseNode.x = coordinateObj["ellipseNodeCoordinateX"];
-    ellipseNode.y = coordinateObj["ellipseNodeCoordinateY"];
-    textNode.x = coordinateObj["textNodeCoordianteX"];
-    textNode.y = coordinateObj["textNodeCoordinateY"];
-    figma.currentPage.appendChild(ellipseNode);
-    figma.currentPage.appendChild(textNode);
-});
 figma.ui.onmessage = (param) => {
     if (param.type === "apply_status") {
         figma.currentPage.selection.forEach(node => {
-            var color = statusColors[param.message.status];
-            generateStatus(node, param.message.status, color, param.message.ticketNumber);
-            if (node.name) {
-                node.name = param.message.status + " " + param.message.ticketNumber;
-            }
+            node.name =
+                setStatusColor(param.message.status) +
+                    param.message.status +
+                    " " +
+                    param.message.ticketNumber;
         });
     }
     figma.closePlugin();
+    figma.notify("Status updated for selected frames successfully");
 };
--- code.ts
+++ code.ts
@@ -1,138 +1,61 @@
+type StatusType =
+  | "Active"
+  | "In-review"
+  | "Needs changes"
+  | "Approved"
+  | "In-development";
+
 interface IMessageType {
   ticketNumber: string;
-  status: string;
+  status: StatusType;
 }
 interface IPluginMessage {
   type: string;
   message: IMessageType;
 }
 
-interface IColorIndex {
-  r: number;
-  g: number;
-  b: number;
-}
-
-interface IStatusColors {
-  [propName: string]: IColorIndex;
-}
-
-interface INodeCoordinates {
-  [propName: string]: number;
-}
-
-var iFrameProps: ShowUIOptions = {
+const iFrameProps: ShowUIOptions = {
   width: 260,
   height: 300
 };
 
-const statusColors: IStatusColors = {
-  Active: {
-    r: 24,
-    g: 160,
-    b: 251
-  },
-  "In-review": {
-    r: 255,
-    g: 199,
-    b: 0
-  },
-  "Needs changes": {
-    r: 242,
-    g: 72,
-    b: 34
-  },
-  Approved: {
-    r: 27,
-    g: 196,
-    b: 125
-  },
-  "In-development": {
-    r: 255,
-    g: 0,
-    b: 255
+const setStatusColor = (statusType: StatusType) => {
+  var emojiHex: string = "";
+
+  switch (statusType) {
+    case "Active":
+      emojiHex = "\uD83D\uDD35";
+      break;
+    case "Approved":
+      emojiHex = "\uD83D\uDFE2";
+      break;
+    case "In-development":
+      emojiHex = "\uD83D\uDFE3";
+      break;
+    case "In-review":
+      emojiHex = "\uD83D\uDFE1";
+      break;
+    case "Needs changes":
+      emojiHex = "\uD83D\uDD34";
+      break;
   }
+
+  return emojiHex;
 };
 
 figma.showUI(__html__, iFrameProps);
 
-const setColors = (fills: any, color: IColorIndex) => {
-  fills[0].color.r = color.r;
-  fills[0].color.g = color.g;
-  fills[0].color.b = color.b;
-  return fills;
-};
-
-const determineCoordinatesPosition = (
-  node: SceneNode,
-  ellipseNode: EllipseNode,
-  textNode: TextNode
-) => {
-  var positionObj: INodeCoordinates = {
-    ellipseNodeCoordinateX:
-      node.x + node.width - textNode.width - ellipseNode.width - 1,
-    ellipseNodeCoordinateY: node.y - ellipseNode.height,
-    textNodeCoordianteX: node.x + node.width - textNode.width,
-    textNodeCoordinateY: node.y - textNode.height
-  };
-
-  return positionObj;
-};
-
-const generateStatus = async (
-  node: SceneNode,
-  status: string,
-  color: IColorIndex,
-  ticketNumber?: string
-) => {
-  await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
-  let textNode = figma.createText();
-  let ellipseNode = figma.createEllipse();
-
-  ellipseNode.resize(7, 7);
-
-  ellipseNode.fills = [
-    {
-      type: "SOLID",
-      color: {
-        r: color.r / 255,
-        g: color.g / 255,
-        b: color.b / 255
-      }
-    }
-  ];
-
-  textNode.characters = status + " " + ticketNumber;
-  textNode.fontSize = 6;
-
-  var coordinateObj = determineCoordinatesPosition(node, ellipseNode, textNode);
-
-  ellipseNode.x = coordinateObj["ellipseNodeCoordinateX"];
-  ellipseNode.y = coordinateObj["ellipseNodeCoordinateY"];
-
-  textNode.x = coordinateObj["textNodeCoordianteX"];
-  textNode.y = coordinateObj["textNodeCoordinateY"];
-
-  figma.currentPage.appendChild(ellipseNode);
-  figma.currentPage.appendChild(textNode);
-};
-
 figma.ui.onmessage = (param: IPluginMessage) => {
   if (param.type === "apply_status") {
     figma.currentPage.selection.forEach(node => {
-      var color = statusColors[param.message.status];
-      generateStatus(
-        node,
-        param.message.status,
-        color,
-        param.message.ticketNumber
-      );
-
-      if (node.name) {
-        node.name = param.message.status + " " + param.message.ticketNumber;
-      }
+      node.name =
+        setStatusColor(param.message.status) +
+        param.message.status +
+        " " +
+        param.message.ticketNumber;
     });
   }
 
   figma.closePlugin();
+  figma.notify("Status updated for selected frames successfully");
 };
